7382a3f9850347cfe7a52d5ea22348787430a479
Bogdan Mircea <bogdan@c.amo.co>
2024-10-17 02:27:35 +0300

    modified rust_project.rs

--- tools/rust_analyzer/rust_project.rs
+++ tools/rust_analyzer/rust_project.rs
@@ -1,52 +1,109 @@
 //! Library for generating rust_project.json files from a `Vec<CrateSpec>`
 //! See official documentation of file format at https://rust-analyzer.github.io/manual.html
 
-use std::collections::{BTreeMap, BTreeSet, HashMap};
+use std::collections::{BTreeMap, BTreeSet};
 use std::io::ErrorKind;
-use std::path::Path;
 
 use anyhow::anyhow;
+use camino::{Utf8Path, Utf8PathBuf};
 use serde::Serialize;
 
-use crate::aquery::{CrateSpec, CrateType};
+use crate::aquery::CrateSpec;
 
 /// A `rust-project.json` workspace representation. See
 /// [rust-analyzer documentation][rd] for a thorough description of this interface.
 /// [rd]: https://rust-analyzer.github.io/manual.html#non-cargo-based-projects
 #[derive(Debug, Serialize)]
 pub struct RustProject {
-    /// The path to a Rust sysroot.
-    sysroot: Option<String>,
-
+    /// Path to the sysroot directory.
+    ///
+    /// The sysroot is where rustc looks for the
+    /// crates that are built-in to rust, such as
+    /// std.
+    ///
+    /// https://doc.rust-lang.org/rustc/command-line-arguments.html#--sysroot-override-the-system-root
+    ///
+    /// To see the current value of sysroot, you
+    /// can query rustc:
+    ///
+    /// ```
+    /// $ rustc --print sysroot
+    /// /Users/yourname/.rustup/toolchains/stable-x86_64-apple-darwin
+    /// ```
+    sysroot: Option<Utf8PathBuf>,
     /// Path to the directory with *source code* of
     /// sysroot crates.
-    sysroot_src: Option<String>,
-
+    ///
+    /// By default, this is `lib/rustlib/src/rust/library`
+    /// relative to the sysroot.
+    ///
+    /// It should point to the directory where std,
+    /// core, and friends can be found:
+    ///
+    /// https://github.com/rust-lang/rust/tree/master/library.
+    ///
+    /// If provided, rust-analyzer automatically adds
+    /// dependencies on sysroot crates. Conversely,
+    /// if you omit this path, you can specify sysroot
+    /// dependencies yourself and, for example, have
+    /// several different "sysroots" in one graph of
+    /// crates.
+    sysroot_src: Option<Utf8PathBuf>,
+    /// List of groups of common cfg values, to allow
+    /// sharing them between crates.
+    ///
+    /// Maps from group name to its cfgs. Cfg follow
+    /// the same format as `Crate.cfg`.
+    cfg_groups: BTreeMap<String, Vec<String>>,
     /// The set of crates comprising the current
     /// project. Must include all transitive
     /// dependencies as well as sysroot crate (libstd,
     /// libcore and such).
     crates: Vec<Crate>,
+
+    pub(crate) runnables: Vec<Runnable>,
 }
 
 /// A `rust-project.json` crate representation. See
 /// [rust-analyzer documentation][rd] for a thorough description of this interface.
 /// [rd]: https://rust-analyzer.github.io/manual.html#non-cargo-based-projects
 #[derive(Debug, Serialize)]
-#[serde(default)]
 pub struct Crate {
     /// A name used in the package's project declaration
     #[serde(skip_serializing_if = "Option::is_none")]
     display_name: Option<String>,
 
     /// Path to the root module of the crate.
-    root_module: String,
+    root_module: Utf8PathBuf,
 
     /// Edition of the crate.
     edition: String,
 
+    /// Crate version
+    version: Option<String>,
+
     /// Dependencies
     deps: Vec<Dependency>,
+
+    #[serde(skip_serializing_if = "BTreeSet::is_empty")]
+    cfg_groups: BTreeSet<String>,
+
+    /// The set of cfgs activated for a given crate, like
+    /// `["unix", "feature=\"foo\"", "feature=\"bar\""]`.
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    cfg: Vec<String>,
+
+    /// Target triple for this Crate.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    target: Option<String>,
+
+    /// Environment variables, used for the `env!` macro
+    #[serde(skip_serializing_if = "BTreeMap::is_empty")]
+    env: BTreeMap<String, String>,
+
+    /// For proc-macro crates, path to compiled proc-macro (.so file).
+    #[serde(skip_serializing_if = "Option::is_none")]
+    proc_macro_dylib_path: Option<String>,
 
     /// Should this crate be treated as a member of current "workspace".
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -56,65 +113,108 @@ pub struct Crate {
     #[serde(skip_serializing_if = "Source::is_empty")]
     source: Source,
 
-    /// The set of cfgs activated for a given crate, like
-    /// `["unix", "feature=\"foo\"", "feature=\"bar\""]`.
-    cfg: Vec<String>,
-
-    /// Target triple for this Crate.
-    #[serde(skip_serializing_if = "Option::is_none")]
-    target: Option<String>,
-
-    /// Environment variables, used for the `env!` macro
-    #[serde(skip_serializing_if = "Option::is_none")]
-    env: Option<BTreeMap<String, String>>,
-
     /// Whether the crate is a proc-macro crate.
     is_proc_macro: bool,
 
-    /// For proc-macro crates, path to compiled proc-macro (.so file).
     #[serde(skip_serializing_if = "Option::is_none")]
-    proc_macro_dylib_path: Option<String>,
+    repository: Option<String>,
 
-    /// Additional, build-specific data about a crate.
     #[serde(skip_serializing_if = "Option::is_none")]
     build: Option<Build>,
 }
 
-#[derive(Debug, Serialize)]
-#[serde(rename_all = "snake_case")]
+#[derive(Clone, Copy, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
 pub enum TargetKind {
     Bin,
+    /// Any kind of Cargo lib crate-type (dylib, rlib, proc-macro, ...).
     Lib,
+    Test,
 }
 
-impl TargetKind {
-    fn new(c: &CrateSpec) -> Self {
-        match c.crate_type {
-            CrateType::Bin => Self::Bin,
-            CrateType::Rlib
-            | CrateType::Lib
-            | CrateType::Dylib
-            | CrateType::Cdylib
-            | CrateType::Staticlib
-            | CrateType::ProcMacro => Self::Lib,
-        }
-    }
+/// A template-like structure for describing runnables.
+///
+/// These are used for running and debugging binaries and tests without encoding
+/// build system-specific knowledge into rust-analyzer.
+///
+/// # Example
+///
+/// Below is an example of a test runnable. `{label}` and `{test_id}`
+/// are explained in [`Runnable::args`]'s documentation.
+///
+/// ```json
+/// {
+///     "program": "buck",
+///     "args": [
+///         "test",
+///          "{label}",
+///          "--",
+///          "{test_id}",
+///          "--print-passing-details"
+///     ],
+///     "cwd": "/home/user/repo-root/",
+///     "kind": "testOne"
+/// }
+/// ```
+#[derive(Debug, Clone, Serialize)]
+pub struct Runnable {
+    /// The program invoked by the runnable.
+    ///
+    /// For example, this might be `cargo`, `buck`, or `bazel`.
+    pub program: String,
+    /// The arguments passed to [`Runnable::program`].
+    ///
+    /// The args can contain two template strings: `{label}` and `{test_id}`.
+    /// rust-analyzer will find and replace `{label}` with [`Build::label`] and
+    /// `{test_id}` with the test name.
+    pub args: Vec<String>,
+    /// The current working directory of the runnable.
+    pub cwd: Utf8PathBuf,
+    pub kind: RunnableKind,
+}
+
+/// The kind of runnable.
+#[derive(Debug, Clone, Copy, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum RunnableKind {
+    Check,
+
+    /// Can run a binary.
+    Run,
+
+    /// Run a single test.
+    TestOne,
 }
 
 #[derive(Debug, Serialize)]
 pub struct Build {
-    /// The name associated with this crate. Build system-specific.
+    /// The name associated with this crate.
+    ///
+    /// This is determined by the build system that produced
+    /// the `rust-project.json` in question. For instance, if buck were used,
+    /// the label might be something like `//ide/rust/rust-analyzer:rust-analyzer`.
+    ///
+    /// Do not attempt to parse the contents of this string; it is a build system-specific
+    /// identifier similar to [`Crate::display_name`].
     pub label: String,
     /// Path corresponding to the build system-specific file defining the crate.
-    pub build_file: String,
+    ///
+    /// It is roughly analogous to [`ManifestPath`], but it should *not* be used with
+    /// [`crate::ProjectManifest::from_manifest_file`], as the build file may not be
+    /// be in the `rust-project.json`.
+    pub build_file: Utf8PathBuf,
     /// The kind of target.
+    ///
+    /// Examples (non-exhaustively) include [`TargetKind::Bin`], [`TargetKind::Lib`],
+    /// and [`TargetKind::Test`]. This information is used to determine what sort
+    /// of runnable codelens to provide, if any.
     pub target_kind: TargetKind,
 }
 
 #[derive(Debug, Default, Serialize)]
 pub struct Source {
-    include_dirs: Vec<String>,
-    exclude_dirs: Vec<String>,
+    include_dirs: Vec<Utf8PathBuf>,
+    exclude_dirs: Vec<Utf8PathBuf>,
 }
 
 impl Source {
@@ -135,6 +235,7 @@ pub struct Dependency {
 }
 
 pub fn generate_rust_project(
+    workspace: &Utf8Path,
     sysroot: &str,
     sysroot_src: &str,
     crates: &BTreeSet<CrateSpec>,
@@ -143,11 +244,31 @@ pub fn generate_rust_project(
         sysroot: Some(sysroot.into()),
         sysroot_src: Some(sysroot_src.into()),
         crates: Vec::new(),
+        cfg_groups: BTreeMap::new(),
+        runnables: vec![
+            Runnable {
+                program: "bazel".to_owned(),
+                args: vec!["build".to_owned(), "{label}".to_owned()],
+                cwd: workspace.to_owned(),
+                kind: RunnableKind::Check,
+            },
+            Runnable {
+                program: "bazel".to_owned(),
+                args: vec![
+                    "test".to_owned(),
+                    "{label}".to_owned(),
+                    "--".to_owned(),
+                    "{test_id}".to_owned(),
+                ],
+                cwd: workspace.to_owned(),
+                kind: RunnableKind::TestOne,
+            },
+        ],
     };
 
     let mut unmerged_crates: Vec<&CrateSpec> = crates.iter().collect();
     let mut skipped_crates: Vec<&CrateSpec> = Vec::new();
-    let mut merged_crates_index: HashMap<String, usize> = HashMap::new();
+    let mut merged_crates_index: BTreeMap<String, usize> = BTreeMap::new();
 
     while !unmerged_crates.is_empty() {
         for c in unmerged_crates.iter() {
@@ -170,7 +291,7 @@ pub fn generate_rust_project(
                 merged_crates_index.insert(c.crate_id.clone(), project.crates.len());
                 project.crates.push(Crate {
                     display_name: Some(c.display_name.clone()),
-                    root_module: c.root_module.clone(),
+                    root_module: c.root_module.clone().into(),
                     edition: c.edition.clone(),
                     deps: c
                         .deps
@@ -202,14 +323,17 @@ pub fn generate_rust_project(
                     },
                     cfg: c.cfg.clone(),
                     target: Some(c.target.clone()),
-                    env: Some(c.env.clone()),
+                    env: c.env.clone(),
                     is_proc_macro: c.proc_macro_dylib_path.is_some(),
                     proc_macro_dylib_path: c.proc_macro_dylib_path.clone(),
                     build: c.build_file.as_ref().map(|build_file| Build {
-                        label: c.bazel_target.as_string(),
-                        build_file: build_file.to_string_lossy().to_string(),
-                        target_kind: TargetKind::new(&c),
+                        label: c.bazel_target.clone(),
+                        build_file: build_file.to_owned(),
+                        target_kind: c.crate_type.into(),
                     }),
+                    version: None,
+                    cfg_groups: BTreeSet::new(),
+                    repository: None,
                 });
             }
         }
@@ -282,19 +406,11 @@ fn detect_cycle<'a>(
 }
 
 pub fn write_rust_project(
-    rust_project_path: &Path,
-    execution_root: &Path,
-    output_base: &Path,
+    rust_project_path: &Utf8Path,
+    execution_root: &Utf8Path,
+    output_base: &Utf8Path,
     rust_project: &RustProject,
 ) -> anyhow::Result<()> {
-    let execution_root = execution_root
-        .to_str()
-        .ok_or_else(|| anyhow!("execution_root is not valid UTF-8"))?;
-
-    let output_base = output_base
-        .to_str()
-        .ok_or_else(|| anyhow!("output_base is not valid UTF-8"))?;
-
     // Try to remove the existing rust-project.json. It's OK if the file doesn't exist.
     match std::fs::remove_file(rust_project_path) {
         Ok(_) => {}
@@ -310,9 +426,9 @@ pub fn write_rust_project(
     // Render the `rust-project.json` file and replace the exec root
     // placeholders with the path to the local exec root.
     let rust_project_content = serde_json::to_string_pretty(rust_project)?
-        .replace("${pwd}", execution_root)
-        .replace("__EXEC_ROOT__", execution_root)
-        .replace("__OUTPUT_BASE__", output_base);
+        .replace("${pwd}", execution_root.as_str())
+        .replace("__EXEC_ROOT__", execution_root.as_str())
+        .replace("__OUTPUT_BASE__", output_base.as_str());
 
     // Write the new rust-project.json file.
     std::fs::write(rust_project_path, rust_project_content)?;
@@ -328,6 +444,7 @@ mod tests {
     #[test]
     fn generate_rust_project_single() {
         let project = generate_rust_project(
+            "",
             "sysroot",
             "sysroot_src",
             &BTreeSet::from([CrateSpec {
@@ -344,6 +461,8 @@ mod tests {
                 env: BTreeMap::new(),
                 target: "x86_64-unknown-linux-gnu".into(),
                 crate_type: "rlib".into(),
+                build_file: None,
+                bazel_target: "//tools/rust_analyzer:gen_rust_project_lib".to_owned(),
             }]),
         )
         .expect("expect success");
@@ -359,6 +478,7 @@ mod tests {
     #[test]
     fn generate_rust_project_with_deps() {
         let project = generate_rust_project(
+            "",
             "sysroot",
             "sysroot_src",
             &BTreeSet::from([
@@ -376,6 +496,8 @@ mod tests {
                     env: BTreeMap::new(),
                     target: "x86_64-unknown-linux-gnu".into(),
                     crate_type: "rlib".into(),
+                    build_file: None,
+                    bazel_target: "//tools/rust_analyzer:gen_rust_project_lib".to_owned(),
                 },
                 CrateSpec {
                     aliases: BTreeMap::new(),
@@ -391,6 +513,8 @@ mod tests {
                     env: BTreeMap::new(),
                     target: "x86_64-unknown-linux-gnu".into(),
                     crate_type: "rlib".into(),
+                    build_file: None,
+                    bazel_target: "//tools/rust_analyzer:gen_rust_project_lib".to_owned(),
                 },
                 CrateSpec {
                     aliases: BTreeMap::new(),
@@ -406,6 +530,8 @@ mod tests {
                     env: BTreeMap::new(),
                     target: "x86_64-unknown-linux-gnu".into(),
                     crate_type: "rlib".into(),
+                    build_file: None,
+                    bazel_target: "//tools/rust_analyzer:gen_rust_project_lib".to_owned(),
                 },
             ]),
         )
